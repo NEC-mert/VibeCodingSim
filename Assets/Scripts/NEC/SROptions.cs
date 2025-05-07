188a28741570a98a0811112ea23f2e74f878b4c9
NEC-mert <mert@notenoughcheddar.com>
2025-05-08 01:04:01 +0300

    Configured sroptions

---
+++ Assets/Scripts/NEC/SROptions.cs
@@ -0,0 +1,11 @@
+using System.ComponentModel;
+using UnityEngine;
+
+public partial class SROptions
+{
+    [Category("Test")]
+    public void TestLog()
+    {
+        Debug.Log("Testing...");
+    }
+}
